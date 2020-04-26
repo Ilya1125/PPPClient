258f05d928f7ea28aa61a5c28ca9f9db70e13c58
PestovIlya <i.pestov1999@mail.ru>
2020-04-26 17:50:03 +0300

    remove database and security

--- controllers/src/main/java/config/SwaggerConfig.java
+++ controllers/src/main/java/config/SwaggerConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -23,30 +21,13 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("controllers"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(metaData())
-                .securityContexts(Collections.singletonList(actuatorSecurityContext()))
-                .securitySchemes(Collections.singletonList(basicAuthScheme()));
-    }
-
-    private SecurityContext actuatorSecurityContext() {
-        return SecurityContext.builder()
-                .securityReferences(Collections.singletonList(basicAuthReference()))
-                .forPaths(PathSelectors.ant("/*"))
-                .build();
-    }
-
-    private SecurityScheme basicAuthScheme() {
-        return new BasicAuth("basicAuth");
-    }
-
-    private SecurityReference basicAuthReference() {
-        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+                .apiInfo(metaData());
     }
 
     private ApiInfo metaData() {
         return new ApiInfo(
                 "Spring Boot REST API",
-                "Spring Boot REST API for Movie theatre",
+                "Spring Boot REST API for PPP",
                 "1.0",
                 "Terms of service",
                 new Contact("Ilya Pestov", "https://", "i.pestov1999@mail.ru"),
